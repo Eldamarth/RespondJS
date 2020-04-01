cd3d596c6ac5c8590df2c348767f606c698fccc7
James Kelley <james.a.kelley.mars@gmail.com>
2020-03-31 20:24:55 -0600

    Build rudimentary Reconciler.diff without keys

--- RespondDOM.js
+++ RespondDOM.js
@@ -1,23 +1,60 @@
 const Reconciler = {
-    
-    //  Takes a virtual element created by Respond.createElement(), a container node, and the old DOM element to replace or update.
-    //  The old DOM element doesn't necessarily exist, i.e. at first call of render method.
-    diff: ()=>{},
-         
+  //  Takes a virtual element created by Respond.createElement(), a container node, and the old DOM element to replace or update.
+  //  The old DOM element doesn't necessarily exist, i.e. at first call of render method.
+  diff: () => {
+    const oldVirtualElement = oldDomElement && oldDomElement._virtualElement;
 
-    updateTextNode: ()=>{},
+    if (oldVirtualElement && oldVirtualElement.type === virtualElement.type) {
+      if (oldVirtualElement.tyoe === "text") {
+        Reconciler.updateTextNode(
+          oldDomElement,
+          virtualElement,
+          oldVirtualElement
+        );
+      } else {
+        Reconciler.updateDomElement(
+          oldDomElement,
+          virtualElement,
+          oldVirtualElement
+        );
+      }
 
+      // saves the virtualElement on the domElement
+      oldDomElement._virtualElement = virtualElement;
 
-    updateDomElement:()=>{},
+      virtualElement.children.forEach((childElemen, i) => {
+        Reconciler.diff(
+          childElement,
+          oldDomElement,
+          oldDomElement.childNodes[i]
+        );
+      });
 
+      // removes excess children
+      const oldChildren = oldDomElement.childNodes;
+      if (oldChildren.length > virtualElement.children.length) {
+        for (
+          let i = oldChildren.length - 1;
+          i >= virtualElement.children.length;
+          i--
+        ) {
+          oldChildren[i].remove();
+        }
+      }
+    } else {
+      Reconciler.mountElement(virtualElement, container, oldDomElement);
+    }
+  },
 
-    mountElement: ()=>{},
+  updateTextNode: () => {},
 
-}
+  updateDomElement: () => {},
 
+  mountElement: () => {}
+};
 
 export default {
-    render: (element, container) => {
-        Reconciler.diff(element,container,container.firstChild);
-    }
+  render: (element, container) => {
+    Reconciler.diff(element, container, container.firstChild);
+  }
 };
