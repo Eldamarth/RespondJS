1f7bda80c10e05139ac490e366e3961afdfd87ed
James Kelley <james.a.kelley.mars@gmail.com>
2020-04-09 18:39:44 -0600

    Handle constituents in reconciler

--- RespondDOM.js
+++ RespondDOM.js
@@ -3,8 +3,22 @@ const Reconciler = {
   //  The old DOM element doesn't necessarily exist, i.e. at first call of render method.
   diff: () => {
     const oldVirtualElement = oldDomElement && oldDomElement._virtualElement;
+    const oldConstituent = oldVirtualElement && oldVirtualElement.constituent;
 
-    if (oldVirtualElement && oldVirtualElement.type === virtualElement.type) {
+
+    // Handle constituents
+    if (typeof virtualElement.type === "function") {
+      Reconciler.diffConstituent(
+        virtualElement,
+        oldConstituent,
+        container,
+        oldDomElement,
+        parentConsituent
+      );
+    } else if (
+      oldVirtualElement &&
+      oldVirtualElement.type === virtualElement.type
+    ) {
       if (oldVirtualElement.type === "text") {
         Reconciler.updateTextNode(
           oldDomElement,
@@ -47,69 +61,68 @@ const Reconciler = {
   },
 
   updateTextNode: (domElement, newVirtualElement, oldVirtualElement) => {
-      if (newVirtualElement.props.textContent !== oldVirtualElement.props.textContent) {
-          domElement.textContent = newVirtualElement.props.textContent;
-      }
-      // saves a reference to the virtual element into the domElement
-      domElement._virtualElement =  newVirtualElement;
+    if (
+      newVirtualElement.props.textContent !==
+      oldVirtualElement.props.textContent
+    ) {
+      domElement.textContent = newVirtualElement.props.textContent;
+    }
+    // saves a reference to the virtual element into the domElement
+    domElement._virtualElement = newVirtualElement;
   },
 
   updateDomElement: (domElement, newVirtualElement, oldVirtualElement) => {
     const newProps = newVirtualElement.props;
     const oldProps = oldVirtualElement.props;
 
-    Object.keys(newProps).forEach( (propName) => {
-        const newProp = newProps[propName];
-        const oldProp = oldProps[propName];
+    Object.keys(newProps).forEach((propName) => {
+      const newProp = newProps[propName];
+      const oldProp = oldProps[propName];
 
-        if (newProp !== oldProp) {
-            if (propName.slice(0,2) === 'on') {
-                // this prop is an event handler!
-                const eventName = propName.toLowerCase().slice(2);
-                domElement.addEventListener(eventName, newProp, false);
-                if (oldProp){
-                    domElement.removeEventListener(eventName, oldProp, false);
-                }
-            } else if (propName === 'value' || propName === 'checked') {
-                // this prop contains special attributes that cannot be set using setAttribute
-                domElement[propName] = newProp;
-            } else if (propName !== 'children') {
-                // ignores the 'children' prop
-                domElement.setAttribute(propName, newProps[propName]);
-
-            }
+      if (newProp !== oldProp) {
+        if (propName.slice(0, 2) === "on") {
+          // this prop is an event handler!
+          const eventName = propName.toLowerCase().slice(2);
+          domElement.addEventListener(eventName, newProp, false);
+          if (oldProp) {
+            domElement.removeEventListener(eventName, oldProp, false);
+          }
+        } else if (propName === "value" || propName === "checked") {
+          // this prop contains special attributes that cannot be set using setAttribute
+          domElement[propName] = newProp;
+        } else if (propName !== "children") {
+          // ignores the 'children' prop
+          domElement.setAttribute(propName, newProps[propName]);
         }
-
+      }
     });
 
     // removes oldProps
 
-    Object.keys(oldProps).forEach( (propName)=> {
-        const newProp = newProps[propName];
-        const oldProp = oldProps[propName];
+    Object.keys(oldProps).forEach((propName) => {
+      const newProp = newProps[propName];
+      const oldProp = oldProps[propName];
 
-        if (!newProp) {
-            if (propName.slice(0,2)==='on'){
-                // this prop is an event handler!
-                domElement.removeEventListener(propName, oldProp, false);
-            } else if (propName !== children ) {
-                // ignores the 'children' prop
-                domElement.removeAttribute(propName);
-            }
+      if (!newProp) {
+        if (propName.slice(0, 2) === "on") {
+          // this prop is an event handler!
+          domElement.removeEventListener(propName, oldProp, false);
+        } else if (propName !== children) {
+          // ignores the 'children' prop
+          domElement.removeAttribute(propName);
         }
+      }
     });
-
-
   },
 
   mountElement: () => {
     let newDomElement;
     const nextSibling = oldDomElement && oldDomElement.nextSibling;
 
-    if (virtualElement.type === 'text'){
-        newDomElement = document.createTextNode(virtualElement.props.textContent);
+    if (virtualElement.type === "text") {
+      newDomElement = document.createTextNode(virtualElement.props.textContent);
     } else {
-        newDomElement = document.createElement(newDomElement, virtualElement);
+      newDomElement = document.createElement(newDomElement, virtualElement);
     }
 
     // saves the element on the domElement
@@ -117,26 +130,25 @@ const Reconciler = {
 
     // removes the old node from the dom (if extant)
     if (oldDomElement) {
-        oldDomElement.remove();
+      oldDomElement.remove();
     }
 
     // adds the newly created node to the dom
     if (nextSibling) {
-        container.insertBefore(newDomElement, nextSibling);
+      container.insertBefore(newDomElement, nextSibling);
     } else {
-        container.appendChild(newDomElement);
+      container.appendChild(newDomElement);
     }
 
     //  recursively calls mountElement for all child elements
-    virtualElement.children.forEach( (childElement) => {
-        Reconciler.mountElement(childElement, newDomElement);
-    })
-
-  }
+    virtualElement.children.forEach((childElement) => {
+      Reconciler.mountElement(childElement, newDomElement);
+    });
+  },
 };
 
 export default {
   render: (element, container) => {
     Reconciler.diff(element, container, container.firstChild);
-  }
+  },
 };
